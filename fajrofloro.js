b80b4ce2e2578fa95f0020f06c89fefa6c7a6cee
Klaus Stein <apps@istik.de>
2020-04-14 14:43:06 +0200

    Improved test environment
    We are still in communication testing.
    Additionally I set the url to meet.jit.si as they may have more capacity.

--- fajrofloro.js
+++ fajrofloro.js
@@ -1,5 +1,5 @@
 
-const api = new JitsiMeetExternalAPI('meet.nimmerland.berlin', {
+const api = new JitsiMeetExternalAPI('meet.jit.si', {
 		roomName: 'fajrofloro',
 		parentNode: document.querySelector('#jitsi'),
 		configOverwrite_: {
@@ -20,14 +20,23 @@ const gamezone = document.getElementById('game');
 
 const participantListDOM = document.getElementById('participantList');
 
+function showDebug(m) {
+		
+}
+
 function participantJoined(p) {
 		const pid = p.id;
+
+		const rowid = 'row-' + pid;
+
+		if(document.getElementById(rowid)) { return; }
 
 		const msgid = 'msg-' + pid;
 		const receivedid = 'received-' + pid;
 		const buttonid = 'button-' + pid;
 
-		const html = '<tr><td>' + p.displayName +
+		const html = '<tr id="' + rowid + 
+					'"><td>' + p.displayName +
 					'</td><td>' + pid +
 					'</td><td><input id="' + msgid +
 					'" type="text" value="xxx" /></td><td><button id="' + buttonid +
@@ -35,11 +44,16 @@ function participantJoined(p) {
 					'"></tr>';
 		
 		participantListDOM.insertAdjacentHTML('beforeend', html);
-		let button = document.getElementById(buttonid);
-		let msg = document.getElementById(msgid);
+		const button = document.getElementById(buttonid);
+		const msg = document.getElementById(msgid);
 		button.addEventListener('click', event => {
-				api.executeCommand('sendEndpointTextMessage',pid,msg.value);
+				api.executeCommand('sendEndpointTextMessage', pid, msg.value);
 		});
+}
+
+function participantLeft(p) {
+		const rowid = 'row-' + p.id;
+		document.getElementById(rowid).remove();
 }
 
 function endpointTextMessageReceived(msg) {
@@ -58,10 +72,26 @@ function outgoingMessage(msg) {
 		alert('outgoing: '+msg.message);
 }
 
-api.on('incomingMessage', incomingMessage);
-api.on('outgoingMessage', outgoingMessage);
 
-api.on('participantJoined', participantJoined);
-api.on('endpointTextMessageReceived', endpointTextMessageReceived);
+function main() {
+		api.on('incomingMessage', incomingMessage);
+		api.on('outgoingMessage', outgoingMessage);
+		
+		api.on('participantJoined', participantJoined);
+		api.on('participantLeft', participantLeft);
+		api.on('endpointTextMessageReceived', endpointTextMessageReceived);
 
+
+		const iframe = document.getElementById('jitsi').firstChild; 
+		function reloadJitsi() {
+				const url = iframe.src;
+				iframe.src='';
+				setTimeout(() => { iframe.src = url; }, 200);
+		}
+
+		const button = document.getElementById('reloadbutton');
+		button.addEventListener('click', reloadJitsi);
+}
 //alert('Ho');
+
+main();
